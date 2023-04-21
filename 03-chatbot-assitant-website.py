0f8b0380ee328345d74a9d409904c62294fea13b
EndyB <endyb.dev@gmail.com>
2023-04-21 19:03:08 +1000

    simple chatbot on web

---
+++ 03-chatbot-assitant-website.py
@@ -0,0 +1,23 @@
+import openai
+import gradio, gradio_client
+
+from core.config import openai_apikey
+
+openai.api_key = openai_apikey
+
+messages = []
+def customChatGPTBot(input: str):
+    messages.append({"role": "assistant", "content": f"{input}"})
+    
+    response = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=messages
+    )
+
+    chatbot_response = response['choices'][0]['message']['content']
+    print(f"\n{chatbot_response}")
+    messages.append({"role": "assistant", "content": f"{chatbot_response}"})
+    return chatbot_response
+
+page = gradio.Interface(fn=customChatGPTBot, inputs="text", outputs="text", title="CustomBot")
+page.launch(share=True)
